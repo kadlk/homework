186328a279936d0897a8f5febd1b7a51c6497768
kadlk <vadimkontush@gmail.com>
2019-03-24 14:39:17 +0300

    add hm_06_01

--- hm_05/hm_05_03/Program.cs
+++ hm_05/hm_05_03/Program.cs
@@ -12,12 +12,16 @@ namespace hm_05_03
         {
             int[] arr = new int[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-            for (int i = 0; i < arr.Length / 2; i++)
+            int i = 0;
+
+            for (; i < arr.Length / 2; i++)
             {
                 int temp = arr[i];
                 arr[i] = arr[arr.Length - 1 - i];
                 arr[arr.Length - 1 - i] = temp;
             }
+
+
 
             foreach (var item in arr)
                 Console.WriteLine(item);
