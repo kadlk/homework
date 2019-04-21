8e5a11d4ccd27d01121da2fce2328ece9d2ccd55
kadlk <vadimkontush@gmail.com>
2019-04-21 16:21:19 +0300

    Made without log4net and DataBase. I will make log4net tomorrow and will try to connect to DB.

---
+++ hm_11/hm_11/hm_11/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace hm_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            Motorcycle honda = new Motorcycle();
+            FillMotoParams(honda);
+            controller.CreateMotorcycle(honda);
+
+            Motorcycle suzuki = new Motorcycle();
+            FillMotoParams(suzuki);
+            controller.CreateMotorcycle(suzuki);
+
+            Motorcycle java = new Motorcycle();
+            FillMotoParams(java);
+            controller.CreateMotorcycle(java);
+
+            Console.WriteLine("Which moto you want to delete. \nType moto id :");
+            controller.DeleteMotorcycle(Console.ReadLine());
+
+            Console.WriteLine("Which moto you want to update. \nType moto id :");
+            Motorcycle temp = new Motorcycle();
+            FillMotoParams(temp);
+            controller.UpdateMotorcycle(Console.ReadLine(), temp);
+
+            for (int i = 0; i < Motorcycle.UsableElements; i++)
+            {
+                Console.WriteLine(Motorcycle.motoArr[i].Id);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void FillMotoParams(Motorcycle motorcycle)
+        {
+            Console.WriteLine("Lets create new moto. \nInput ID: ");
+            motorcycle.Id = Console.ReadLine();
+            Console.WriteLine("Input name: ");
+            motorcycle.Name = Console.ReadLine();
+            Console.WriteLine("Input model: ");
+            motorcycle.Model = Console.ReadLine();
+            Console.WriteLine("Input year: ");
+
+            bool success = Int32.TryParse(Console.ReadLine(), out int number);
+            while (!success)
+            {
+                Console.WriteLine("Input one more time year: ");
+                success = Int32.TryParse(Console.ReadLine(), out number);
+            }
+            motorcycle.Year = number;
+
+            Console.WriteLine("Input odometr: ");
+            success = Int32.TryParse(Console.ReadLine(), out number);
+            while (!success)
+            {
+                Console.WriteLine("Input odometr: ");
+                success = Int32.TryParse(Console.ReadLine(), out number);
+            }
+            motorcycle.Odometer = number;
+        }
+    }
+
+    public class Motorcycle
+    {
+        public static Motorcycle[] motoArr = new Motorcycle[10];
+        public static int UsableElements { get; set; } = 0;
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometer { get; set; }
+    }
+
+    public interface IControl
+    {
+        Motorcycle GetMotorcycleByID(string id);
+        Motorcycle[] GetMotorcycles();
+        void CreateMotorcycle(Motorcycle motorcycle);
+        void UpdateMotorcycle(string id, Motorcycle motorcycle);
+        void DeleteMotorcycle(string id);
+    }
+
+    class Controller : IControl
+    {
+        public void CreateMotorcycle(Motorcycle motorcycle)
+        {
+            Motorcycle.motoArr[Motorcycle.UsableElements] = motorcycle;
+            Motorcycle.UsableElements++;
+        }
+
+        public void DeleteMotorcycle(string id) // Delete moto with shifting left all array
+        {
+            for (int i = 0; i < Motorcycle.UsableElements; i++)
+            {
+                if (id.Equals(Motorcycle.motoArr[i].Id))
+                {
+                    for (int j = i; j < Motorcycle.UsableElements; j++)
+                    {
+                        Motorcycle.motoArr[j] = Motorcycle.motoArr[j + 1];
+                    }
+                    Motorcycle.UsableElements--;
+                }
+            }
+        }
+
+        public Motorcycle GetMotorcycleByID(string id)
+        {
+            for (int i = 0; i < Motorcycle.UsableElements; i++)
+            {
+                if (id.Equals(Motorcycle.motoArr[i].Id))
+                {
+                    return Motorcycle.motoArr[i];
+                }
+            }
+            return null;
+        }
+
+        public Motorcycle[] GetMotorcycles()
+        {
+            return Motorcycle.motoArr;
+        }
+
+        public void UpdateMotorcycle(string id, Motorcycle motorcycle)
+        {
+            for (int i = 0; i < Motorcycle.UsableElements; i++)
+            {
+                if (id.Equals(Motorcycle.motoArr[i].Id))
+                {
+                    Motorcycle.motoArr[i] = motorcycle;
+                }
+            }
+        }
+    }
+}
